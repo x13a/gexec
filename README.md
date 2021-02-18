3c8535cbb606cd9518fdc8ae719e19b0aaf87b7a
lucky <>
2021-02-18 23:02:06 +0300

    add signature check

--- README.md
+++ README.md
@@ -1,13 +1,9 @@
 # exec-guard
 
-Binary execution guard.
+Execution guard.
 
 - Create in-memory file descriptor via memfd_create()
-- Loop
-    - Read a chunk of executable
-    - Write to SHA256 hasher
-    - Write to in-memory file descriptor
-- Checksum
+- Check minisign or sha256
 - Exec via fexecve()
 
 ## Installation
@@ -23,22 +19,44 @@ $ brew install x13a/tap/exec-guard
 
 ## Usage
 ```text
-exec-guard [-h|V] [-e SHA256] [-s SHA256] <EXECUTABLE_PATH> [<SCRIPT_PATH>] [..ARGS]
+exec-guard [-h|V] [-x] [-P PUBLIC_KEY]
+           [-E SIG_PATH] [-e SHA256]
+           [-S SIG_PATH] [-s SHA256]
+           <EXECUTABLE_PATH> [<SCRIPT_PATH>] [..ARGS]
 
 [-h] * Print help and exit
 [-V] * Print version and exit
+[-x] * Use execve()
+[-P] * Minisign base64 public key
+[-E] * Executable signature path
 [-e] * Executable hash
+[-S] * Script signature path
 [-s] * Script hash
 ```
 
 ## Example
 
-To check and exec binary:
+To check and exec binary (sha256):
 ```sh
 $ exec-guard -e "68c1f856c32e521cc04d3d8f28a548c3e66e26b64d25ee10e907dd9b68fdc1c9" /usr/bin/uname -a
 ```
 
-To check and exec script:
+To check and exec script (sha256):
 ```sh
-$ exec-guard -e "EXECUTABLE_SHA256" -s "SCRIPT_SHA256" /usr/bin/python /path/to/file.py
+$ exec-guard -x -s "SCRIPT_SHA256" /usr/bin/python /path/to/file.py
+```
+
+To check and exec binary (minisign):
+```sh
+$ exec-guard -P "MINISIGN_BASE64_PUBLIC_KEY" /usr/bin/true
+```
+
+To exec binary from memory:
+```sh
+$ cat /usr/bin/uname | exec-guard -
+```
+
+To check and exec script from memory (sha256):
+```sh
+$ cat /path/to/file.py | exec-guard -x -s "SCRIPT_SHA256" /usr/bin/python -
 ```
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::convert::Infallible;
+use std::convert::{Infallible, TryFrom};
 use std::env;
 use std::error;
 use std::ffi::CString;
@@ -10,8 +10,9 @@ use std::path::{Path, PathBuf};
 use std::process::exit;
 use std::result;
 
+use minisign_verify::{PublicKey, Signature};
 use nix::{fcntl, unistd};
-use palaver::file::{fexecve, memfd_create, pipe, seal_fd};
+use palaver::file::{execve, fexecve, memfd_create, pipe, seal_fd};
 use sha2::{Digest, Sha256};
 
 const EXIT_SUCCESS: i32 = 0;
@@ -20,7 +21,11 @@ const EXIT_USAGE: i32 = 2;
 mod flag {
     pub const HELP: &'static str = "h";
     pub const VERSION: &'static str = "V";
+    pub const EXEC: &'static str = "x";
+    pub const PUBLIC_KEY: &'static str = "P";
+    pub const EXECUTABLE_SIG_PATH: &'static str = "E";
     pub const EXECUTABLE_HASH: &'static str = "e";
+    pub const SCRIPT_SIG_PATH: &'static str = "S";
     pub const SCRIPT_HASH: &'static str = "s";
 }
 
@@ -30,20 +35,35 @@ enum PrintDestination {
 }
 
 fn print_usage(to: PrintDestination) {
+    let prog_name = PathBuf::from(env::args_os().next().unwrap())
+        .file_name()
+        .unwrap()
+        .to_string_lossy()
+        .to_string();
     let usage = format!(
-        "{P} [-{h}|{V}] [-{e} SHA256] [-{s} SHA256] <EXECUTABLE_PATH> [<SCRIPT_PATH>] [..ARGS]\n\n\
+        "{N} [-{h}|{V}] [-{x}] [-{P} PUBLIC_KEY]\n\
+         {w:width$} [-{E} SIG_PATH] [-{e} SHA256]\n\
+         {w:width$} [-{S} SIG_PATH] [-{s} SHA256]\n\
+         {w:width$} <EXECUTABLE_PATH> [<SCRIPT_PATH>] [..ARGS]\n\n\
          [-{h}] * Print help and exit\n\
          [-{V}] * Print version and exit\n\
+         [-{x}] * Use execve()\n\
+         [-{P}] * Minisign base64 public key\n\
+         [-{E}] * Executable signature path\n\
          [-{e}] * Executable hash\n\
+         [-{S}] * Script signature path\n\
          [-{s}] * Script hash",
-        P = PathBuf::from(env::args_os().next().unwrap())
-            .file_name()
-            .unwrap()
-            .to_string_lossy(),
+        N = prog_name,
         h = flag::HELP,
         V = flag::VERSION,
+        x = flag::EXEC,
+        P = flag::PUBLIC_KEY,
+        E = flag::EXECUTABLE_SIG_PATH,
         e = flag::EXECUTABLE_HASH,
+        S = flag::SCRIPT_SIG_PATH,
         s = flag::SCRIPT_HASH,
+        w = "",
+        width = prog_name.chars().count(),
     );
     match to {
         PrintDestination::Stdout => println!("{}", usage),
@@ -55,7 +75,11 @@ type Result<T> = result::Result<T, Box<dyn error::Error>>;
 
 #[derive(Default)]
 struct Opts {
+    exec: bool,
+    public_key: Option<PublicKey>,
+    executable_sig_path: Option<PathBuf>,
     executable_hash: Option<String>,
+    script_sig_path: Option<PathBuf>,
     script_hash: Option<String>,
     executable_path: PathBuf,
     script_path: Option<PathBuf>,
@@ -74,11 +98,7 @@ fn get_opts() -> Result<Opts> {
         exit(EXIT_USAGE);
     }
     let mut opts = Opts::default();
-    loop {
-        let arg = match argv.next() {
-            Some(s) => s,
-            None => break,
-        };
+    while let Some(arg) = argv.next() {
         if !arg.starts_with('-') || arg == "-" {
             opts.executable_path = arg.into();
             break;
@@ -92,9 +112,22 @@ fn get_opts() -> Result<Opts> {
                 println!("{}", env!("CARGO_PKG_VERSION"));
                 exit(EXIT_SUCCESS);
             }
+            flag::EXEC => opts.exec = true,
+            flag::PUBLIC_KEY => match argv.next() {
+                Some(s) => opts.public_key = Some(PublicKey::from_base64(&s)?),
+                None => exit_usage("missing public key"),
+            },
+            flag::EXECUTABLE_SIG_PATH => match argv.next() {
+                Some(s) => opts.executable_sig_path = Some(s.into()),
+                None => exit_usage("missing executable signature"),
+            },
             flag::EXECUTABLE_HASH => match argv.next() {
                 Some(s) => opts.executable_hash = Some(s),
                 None => exit_usage("missing executable hash"),
+            },
+            flag::SCRIPT_SIG_PATH => match argv.next() {
+                Some(s) => opts.script_sig_path = Some(s.into()),
+                None => exit_usage("missing script signature"),
             },
             flag::SCRIPT_HASH => match argv.next() {
                 Some(s) => opts.script_hash = Some(s),
@@ -103,19 +136,10 @@ fn get_opts() -> Result<Opts> {
             _ => {}
         }
     }
-    if opts.executable_path.as_os_str().is_empty() {
-        exit_usage("empty executable path");
+    if !opts.executable_path.is_file() && !opts.executable_path.is_stdin() {
+        exit_usage("invalid executable path");
     }
-    let hash_len = Sha256::output_size() << 1;
-    if let Some(s) = &opts.executable_hash {
-        if s.len() != hash_len {
-            exit_usage("invalid executable hash");
-        }
-    }
-    if let Some(s) = &opts.script_hash {
-        if s.len() != hash_len {
-            exit_usage("invalid script hash");
-        }
+    if opts.script_sig_path.is_some() || opts.script_hash.is_some() {
         match argv.next() {
             Some(s) => opts.script_path = Some(s.into()),
             None => exit_usage("missing script path"),
@@ -125,19 +149,64 @@ fn get_opts() -> Result<Opts> {
     Ok(opts)
 }
 
-trait IsStdin {
+trait PathExt {
     fn is_stdin(&self) -> bool;
+    fn with_sig_extension(&self) -> Option<PathBuf>;
+    fn read(&self) -> Result<Vec<u8>>;
 }
 
-impl IsStdin for Path {
+impl PathExt for Path {
     fn is_stdin(&self) -> bool {
         self == Path::new("-")
+    }
+
+    fn with_sig_extension(&self) -> Option<PathBuf> {
+        Some(self.with_file_name(format!("{}.minisig", self.file_name()?.to_str()?)))
+    }
+
+    fn read(&self) -> Result<Vec<u8>> {
+        let mut data = Vec::new();
+        if self.is_stdin() {
+            io::stdin().read_to_end(&mut data)?;
+        } else {
+            data = fs::read(self)?;
+        }
+        Ok(data)
+    }
+}
+
+trait SignatureExt {
+    fn from_path(p: impl AsRef<Path>) -> Result<Signature>;
+}
+
+impl SignatureExt for Signature {
+    fn from_path(p: impl AsRef<Path>) -> Result<Signature> {
+        let p = p.as_ref();
+        Ok(if p.is_stdin() {
+            let mut s = String::new();
+            io::stdin().read_to_string(&mut s)?;
+            Signature::decode(&s)?
+        } else {
+            Signature::from_file(p)?
+        })
     }
 }
 
 enum Input {
     Stdin(Stdin),
     File(File),
+}
+
+impl TryFrom<&Path> for Input {
+    type Error = io::Error;
+
+    fn try_from(value: &Path) -> result::Result<Self, Self::Error> {
+        Ok(if value.is_stdin() {
+            Input::Stdin(io::stdin())
+        } else {
+            Input::File(File::open(value)?)
+        })
+    }
 }
 
 impl Read for Input {
@@ -157,82 +226,175 @@ fn decode_hex(s: impl AsRef<str>) -> result::Result<Vec<u8>, ParseIntError> {
         .collect()
 }
 
-fn exec<S1, S2, S3, T>(path: S1, args: T, hash: Option<S3>) -> Result<Infallible>
+fn verify_signature<P, V>(pk: &PublicKey, path: P, data: V) -> Result<()>
 where
-    S1: AsRef<Path>,
-    S2: AsRef<str>,
-    S3: AsRef<str>,
-    T: IntoIterator<Item = S2>,
+    P: AsRef<Path>,
+    V: AsRef<[u8]>,
 {
-    let path = path.as_ref();
-    let mut file1 = if path.is_stdin() {
-        Input::Stdin(io::stdin())
-    } else {
-        Input::File(File::open(path)?)
-    };
-    let fd = memfd_create(&CString::new("")?, true)?;
-    let mut file2 = unsafe { File::from_raw_fd(fd) };
-    if let Some(hash) = &hash {
-        let raw_hash = decode_hex(hash.as_ref())?;
-        let mut buf = [0; 1 << 13];
-        let mut hasher = Sha256::new();
-        loop {
-            let n = match file1.read(&mut buf) {
-                Ok(0) => {
-                    if hasher.finalize()[..] != raw_hash {
-                        return Err("executable hash mismatch".into());
-                    }
-                    break;
+    pk.verify(data.as_ref(), &Signature::from_path(path)?)?;
+    Ok(())
+}
+
+fn copy_hash<R: ?Sized, W: ?Sized, S>(r: &mut R, w: &mut W, hash: S) -> Result<u64>
+where
+    R: Read,
+    W: Write,
+    S: AsRef<str>,
+{
+    let raw_hash = decode_hex(hash.as_ref())?;
+    let mut buf = [0; 1 << 13];
+    let mut hasher = Sha256::new();
+    let mut res = 0;
+    loop {
+        let n = match r.read(&mut buf) {
+            Ok(0) => {
+                if hasher.finalize()[..] != raw_hash {
+                    return Err("executable hash mismatch".into());
                 }
-                Ok(n) => n,
-                Err(ref err) if err.kind() == io::ErrorKind::Interrupted => continue,
-                Err(err) => return Err(err.into()),
-            };
-            let data = &buf[..n];
-            hasher.write_all(data)?;
-            file2.write_all(data)?;
-        }
-    } else {
-        io::copy(&mut file1, &mut file2)?;
+                break;
+            }
+            Ok(n) => n,
+            Err(ref err) if err.kind() == io::ErrorKind::Interrupted => continue,
+            Err(err) => return Err(err.into()),
+        };
+        let data = &buf[..n];
+        hasher.write_all(data)?;
+        w.write_all(data)?;
+        res += n as u64;
     }
-    seal_fd(fd);
+    Ok(res)
+}
+
+fn make_exec_args_vars<P, S, T>(path: P, args: T) -> Result<(Vec<CString>, Vec<CString>)>
+where
+    P: AsRef<Path>,
+    S: AsRef<str>,
+    T: IntoIterator<Item = S>,
+{
     let mut args_c = Vec::with_capacity(1);
-    args_c.push(CString::new(path.to_str().unwrap())?);
+    args_c.push(CString::new(path.as_ref().to_str().ok_or("invalid path")?)?);
     for arg in args {
         args_c.push(CString::new(arg.as_ref())?);
     }
-    let args_c = args_c.iter().map(AsRef::as_ref).collect::<Vec<_>>();
     let mut vars_c = Vec::new();
     for var in env::vars() {
         vars_c.push(CString::new(format!("{}={}", var.0, var.1).as_str())?);
     }
+    Ok((args_c, vars_c))
+}
+
+fn select_sig_path<P1, P2>(sig_path: Option<P1>, path: P2) -> Result<PathBuf>
+where
+    P1: AsRef<Path>,
+    P2: AsRef<Path>,
+{
+    if let Some(sig_path) = sig_path {
+        return Ok(sig_path.as_ref().into());
+    }
+    Ok(path
+        .as_ref()
+        .with_sig_extension()
+        .ok_or("invalid sig path")?)
+}
+
+struct Params<'a> {
+    pk: &'a Option<PublicKey>,
+    sig_path: Option<PathBuf>,
+    hash: Option<String>,
+}
+
+fn fexec<P, S, T>(path: P, args: T, params: Params) -> Result<Infallible>
+where
+    P: AsRef<Path>,
+    S: AsRef<str>,
+    T: IntoIterator<Item = S>,
+{
+    let path = path.as_ref();
+    let mut file1 = Input::try_from(path)?;
+    let fd = memfd_create(&CString::new("")?, true)?;
+    let mut file2 = unsafe { File::from_raw_fd(fd) };
+    if let Some(pk) = params.pk {
+        let mut data = Vec::new();
+        io::copy(&mut file1, &mut data)?;
+        verify_signature(pk, select_sig_path(params.sig_path, path)?, &data)?;
+        file2.write_all(&data)?;
+    } else if let Some(hash) = params.hash {
+        copy_hash(&mut file1, &mut file2, hash)?;
+    } else {
+        io::copy(&mut file1, &mut file2)?;
+    }
+    seal_fd(fd);
+    let (args_c, vars_c) = make_exec_args_vars(path, args)?;
+    let args_c = args_c.iter().map(AsRef::as_ref).collect::<Vec<_>>();
     let vars_c = vars_c.iter().map(AsRef::as_ref).collect::<Vec<_>>();
     fexecve(fd, &args_c, &vars_c)?;
     panic!("never happen");
 }
 
-fn main() -> Result<()> {
-    let opts = get_opts()?;
-    if let Some(s) = &opts.script_hash {
-        assert!(opts.script_path.is_some());
-        let raw_hash = decode_hex(s)?;
-        let mut data = Vec::new();
-        let mut stdin = io::stdin();
-        let path = &opts.script_path.unwrap();
-        if path.is_stdin() {
-            stdin.read_to_end(&mut data)?;
-        } else {
-            data = fs::read(path)?;
-        }
-        if Sha256::digest(&data)[..] != raw_hash {
+fn exec<P, S, T>(path: P, args: T) -> Result<Infallible>
+where
+    P: AsRef<Path>,
+    S: AsRef<str>,
+    T: IntoIterator<Item = S>,
+{
+    let path = path.as_ref();
+    let (args_c, vars_c) = make_exec_args_vars(path, args)?;
+    let args_c = args_c.iter().map(AsRef::as_ref).collect::<Vec<_>>();
+    let vars_c = vars_c.iter().map(AsRef::as_ref).collect::<Vec<_>>();
+    execve(
+        CString::new(path.to_str().ok_or("invalid path")?)?.as_ref(),
+        &args_c,
+        &vars_c,
+    )?;
+    panic!("never happen");
+}
+
+fn process_script<P>(path: Option<P>, params: Params) -> Result<()>
+where
+    P: AsRef<Path>,
+{
+    let path = match path {
+        Some(s) => s,
+        None => return Ok(()),
+    };
+    let data = path.as_ref().read()?;
+    if let Some(pk) = params.pk {
+        verify_signature(pk, select_sig_path(params.sig_path, path)?, &data)?;
+    } else if let Some(hash) = params.hash {
+        if Sha256::digest(&data)[..] != decode_hex(hash)? {
             return Err("script hash mismatch".into());
         }
-        let (pr, pw) = pipe(fcntl::OFlag::O_CLOEXEC)?;
-        unistd::dup2(pr, stdin.as_raw_fd())?;
-        let mut file = unsafe { File::from_raw_fd(pw) };
-        file.write_all(&data)?;
     }
-    exec(&opts.executable_path, &opts.args, opts.executable_hash)?;
+    let (pr, pw) = pipe(fcntl::OFlag::O_CLOEXEC)?;
+    unistd::dup2(pr, io::stdin().as_raw_fd())?;
+    let mut file = unsafe { File::from_raw_fd(pw) };
+    file.write_all(&data)?;
+    Ok(())
+}
+
+fn main() -> Result<()> {
+    let opts = get_opts()?;
+    process_script(
+        opts.script_path,
+        Params {
+            pk: &opts.public_key,
+            sig_path: opts.script_sig_path,
+            hash: opts.script_hash,
+        },
+    )?;
+    if opts.exec {
+        exec(opts.executable_path, &opts.args)?;
+    } else {
+        fexec(
+            opts.executable_path,
+            &opts.args,
+            Params {
+                pk: &opts.public_key,
+                sig_path: opts.executable_sig_path,
+                hash: opts.executable_hash,
+            },
+        )?;
+    }
     Ok(())
 }
 
@@ -242,24 +404,36 @@ mod tests {
 
     #[test]
     #[cfg(target_os = "macos")]
-    fn exec_ok() {
+    fn fexec_ok() {
         let args: &[String] = &[];
-        assert!(!exec(
+        assert!(!fexec(
             "/usr/bin/true",
             args,
-            Some("a7ca60660f08c3907fdf383afc360dd9a0a18da9623ce9d7b2852451ac2dfa5e"),
+            Params {
+                pk: &None,
+                sig_path: None,
+                hash: Some(
+                    "a7ca60660f08c3907fdf383afc360dd9a0a18da9623ce9d7b2852451ac2dfa5e".into()
+                ),
+            }
         )
         .is_err());
     }
 
     #[test]
     #[cfg(target_os = "macos")]
-    fn exec_err() {
+    fn fexec_err() {
         let args: &[String] = &[];
-        assert!(exec(
+        assert!(fexec(
             "/usr/bin/true",
             args,
-            Some("68c1f856c32e521cc04d3d8f28a548c3e66e26b64d25ee10e907dd9b68fdc1c9"),
+            Params {
+                pk: &None,
+                sig_path: None,
+                hash: Some(
+                    "68c1f856c32e521cc04d3d8f28a548c3e66e26b64d25ee10e907dd9b68fdc1c9".into()
+                ),
+            }
         )
         .is_err());
     }
